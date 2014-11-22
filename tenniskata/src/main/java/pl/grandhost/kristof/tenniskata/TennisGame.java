9f9fa679af8095af36bf09507a592de5afca9ad4
kbiele <krzysztof.bielecki00@gmail.com>
2014-11-23 00:43:11 +0100

    green phase - adding points method and logic added
    Method and it's logic that adds point to the first player that is sent to
    it was added.

--- tenniskata/src/main/java/pl/grandhost/kristof/tenniskata/TennisGame.java
+++ tenniskata/src/main/java/pl/grandhost/kristof/tenniskata/TennisGame.java
@@ -12,6 +12,10 @@ public class TennisGame {
 		playerB = new Player();
 	}
 	
+	/**
+	 * Returns current game score
+	 * @return
+	 */
 	public String getCurrentScore() {
 		String result = "";
 		if(playerA.getPoint().equals("deuce")){
@@ -21,17 +25,58 @@ public class TennisGame {
 		}
 		return result;
 	}
-
+	
+	/**
+	 * Increase player A score
+	 */
 	public void playerAScores() {
-		// TODO Auto-generated method stub
-		
+		addPoint(playerA, playerB);
 	}
-
+	
+	/**
+	 * Increase player B score 
+	 */
 	public void playerBScores() {
-		// TODO Auto-generated method stub
-		
+		addPoint(playerB, playerA);
 	}
-
+	
+	/**
+	 * First Player object that is sent to the method wins the ball
+	 * and his score is increased 
+	 * 
+	 * @param playerThatWinTheBall
+	 * @param playerThatLoseTheBall
+	 */
+	private void addPoint(Player playerThatWinTheBall, Player playerThatLoseTheBall){
+		
+		if(playerThatWinTheBall.getPoint().equals("A") || 
+				playerThatWinTheBall.getPoint().equals("40")){
+			playerThatWinTheBall.setPoint("W");
+			playerThatLoseTheBall.setPoint("-");
+		}else if(playerThatWinTheBall.getPoint().equals("30") &&
+			playerThatLoseTheBall.getPoint().equals("40")){
+				playerThatWinTheBall.setPoint("deuce");
+				playerThatLoseTheBall.setPoint("deuce");
+		}else if(playerThatWinTheBall.getPoint().equals("deuce")||
+				playerThatWinTheBall.getPoint().equals("-")){
+			if(playerThatLoseTheBall.getPoint().equals("A")){
+				playerThatWinTheBall.setPoint("deuce");
+				playerThatLoseTheBall.setPoint("deuce");
+			}else{
+				playerThatWinTheBall.setPoint("A");
+				playerThatLoseTheBall.setPoint("-");
+			}
+		}else{
+			String point = playerThatWinTheBall.getPoint();
+			if(point.equals("0")){
+				playerThatWinTheBall.setPoint("15");
+			}else if(point.equals("15")){
+				playerThatWinTheBall.setPoint("30");
+			}else {
+				playerThatWinTheBall.setPoint("40");
+			}
+		}
+	}
 	public ArrayList<String> playTheGame() {
 		// TODO Auto-generated method stub
 		return null;
