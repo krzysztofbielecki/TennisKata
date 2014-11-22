6b798a8481c504f09757137655870314ec995aa2
kbiele <krzysztof.bielecki00@gmail.com>
2014-11-22 23:23:23 +0100

    red phase - tennis game test scenarios extended
    I've added scenarios that are supposed to test major result changes

--- tenniskata/src/test/java/pl/grandhost/kristof/tenniskata/TennisGameTest.java
+++ tenniskata/src/test/java/pl/grandhost/kristof/tenniskata/TennisGameTest.java
@@ -25,5 +25,70 @@ public class TennisGameTest {
 		tg.playerBScores();
 		assertTrue("0 : 15".equals(tg.getCurrentScore()));
 	}
+	
+	@Test
+	public void testing_playerAScores_method_till_winning_the_game(){
+		TennisGame tg = new TennisGame();
+		tg.playerAScores();
+		assertTrue("15 : 0".equals(tg.getCurrentScore()));
+		tg.playerAScores();
+		assertTrue("30 : 0".equals(tg.getCurrentScore()));
+		tg.playerAScores();
+		assertTrue("40 : 0".equals(tg.getCurrentScore()));
+		tg.playerAScores();
+		assertTrue("W : -".equals(tg.getCurrentScore()));
+	}
+	
+	@Test
+	public void testing_playerBScores_method_till_winning_the_game(){
+		TennisGame tg = new TennisGame();
+		tg.playerBScores();
+		assertTrue("0 : 15".equals(tg.getCurrentScore()));
+		tg.playerBScores();
+		assertTrue("0 : 30".equals(tg.getCurrentScore()));
+		tg.playerBScores();
+		assertTrue("0 : 40".equals(tg.getCurrentScore()));
+		tg.playerBScores();
+		assertTrue("- : W".equals(tg.getCurrentScore()));
+	}
+	
+	@Test
+	public void testing_deuce_from_30_40(){
+		TennisGame tg = new TennisGame();
+		tg.playerBScores();
+		tg.playerBScores();
+		tg.playerBScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		assertTrue("deuce".equals(tg.getCurrentScore()));
+	}
+	
+	@Test
+	public void testing_advantage(){
+		TennisGame tg = new TennisGame();
+		tg.playerBScores();
+		tg.playerBScores();
+		tg.playerBScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		assertTrue("A : -".equals(tg.getCurrentScore()));
+	}
+	
+	@Test
+	public void testing_return_to_deuce_from_player_b_advantage(){
+		TennisGame tg = new TennisGame();
+		tg.playerBScores();
+		tg.playerBScores();
+		tg.playerBScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		tg.playerAScores();
+		tg.playerBScores();
+		tg.playerAScores();
+		assertTrue("deuce".equals(tg.getCurrentScore()));
+	}
 
 }
