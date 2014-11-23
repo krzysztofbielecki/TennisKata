7957736fd31b459f6fdc9ca51cebce99a50d78f4
kbiele <krzysztof.bielecki00@gmail.com>
2014-11-23 02:38:28 +0100

    refactor phase - init added to PointUtilTest
    initialization state is added to PointUtilTest

--- tenniskata/src/test/java/pl/grandhost/kristof/tenniskata/PointUtilTest.java
+++ tenniskata/src/test/java/pl/grandhost/kristof/tenniskata/PointUtilTest.java
@@ -2,20 +2,26 @@ package pl.grandhost.kristof.tenniskata;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PointUtilTest {
 
+	private Player a;
+	
+	@Before
+	public void init(){
+		a = new Player();
+	}
+	
 	@Test
 	public void check_next_point_after_0() {
-		Player a = new Player();
 		a.setPoint(PointUtil.getNextPoint(a.getPoint()));
 		assertTrue(PointUtil.FIFTEN.equals(a.getPoint()));
 	}
 	
 	@Test
 	public void check_next_point_after_15() {
-		Player a = new Player();
 		a.setPoint(PointUtil.getNextPoint(a.getPoint()));
 		a.setPoint(PointUtil.getNextPoint(a.getPoint()));
 		assertTrue(PointUtil.THIRTY.equals(a.getPoint()));
@@ -23,7 +29,6 @@ public class PointUtilTest {
 	
 	@Test
 	public void check_next_point_after_30() {
-		Player a = new Player();
 		a.setPoint(PointUtil.getNextPoint(a.getPoint()));
 		a.setPoint(PointUtil.getNextPoint(a.getPoint()));
 		a.setPoint(PointUtil.getNextPoint(a.getPoint()));
